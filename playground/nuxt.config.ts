539b2b301a87907e9a83de9d7649f56e7976f4ca
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-09-29 17:06:28 +0530

    module init

---
+++ playground/nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  modules: ["../src/module"],
+  feedback: {},
+  devtools: { enabled: true },
+});
---
+++ test/fixtures/basic/nuxt.config.ts
@@ -0,0 +1,5 @@
+import feedback from "../../../src/module";
+
+export default defineNuxtConfig({
+  modules: [feedback],
+});
