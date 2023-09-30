9bb31fa953aef9a4c3b76329ba614dae9b7af9ce
AAYUSH TALREJA <2020.aayush.talreja@ves.ac.in>
2023-09-30 21:35:16 +0530

    (add): Feedback Form Added (Static)

---
+++ src/runtime/components/FormDynamic.vue
@@ -0,0 +1,16 @@
+
+
+<template>
+  <div class="flex flex-col gap-4 p-8 bg-black/30 rounded-lg shadow-lg">
+    <div>
+     
+    </div>
+   
+  </div>
+</template>
+  
+<script lang="ts">
+</script>
+  
+<style scoped></style>
+  
