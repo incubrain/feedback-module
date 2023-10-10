23c51531ab5fac3a4154204468d481ae55256ff1
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-10-10 20:09:31 +0530

    chore(refactor): use script setup, simplified code

--- src/runtime/components/FeedbackWidget.vue
+++ src/runtime/components/FeedbackWidget.vue
@@ -1,34 +1,65 @@
 <template>
-  <div class="flex flex-col gap-4 p-8 bg-black/30 rounded-lg shadow-lg">
-    <div>
-      <slot />
-    </div>
-    <UButton @click="github.getUser">Test GH</UButton>
-    <span> {{ github.user }}</span>
+  <div>
+    <slot
+      name="toggle"
+      v-bind="toggle"
+    />
+    <USlideover
+      v-model="isOpen"
+      :ui="{ padding: 'p-4 lg:p-8' }"
+    >
+      <UForm
+        class="flex flex-col gap-6"
+        @submit="submitFeedback"
+      >
+        <slot name="cta" />
+        <UInput
+          v-model="feedback.title"
+          placeholder="Title"
+        />
+        <UTextarea
+          v-model="feedback.body"
+          placeholder="Body"
+        />
+        <UButton type="submit"> Submit </UButton>
+      </UForm>
+    </USlideover>
   </div>
 </template>
 
-<script lang="ts">
-import { ref, computed, defineComponent, toRaw, toRef } from 'vue'
-import useGithub from '../composables/useGithub'
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import { useFeedback } from '../composables/useFeedback'
+// !TODO: add validation and state https://ui.nuxt.com/forms/form
 
-export default defineComponent({
-  props: {
-    title: {
-      type: String,
-      default: 'Feedback'
-    }
-  },
-  // emits: ["update:modelValue"],
-  setup(props, { emit, attrs: $attrs }) {
-    // perform most logic here
-    const github = useGithub()
-
-    return {
-      github
-    }
-  }
+const isOpen = ref(false)
+const feedback = reactive({
+  title: '',
+  body: ''
 })
+
+function toggle() {
+  isOpen.value = !isOpen.value
+}
+
+function submitFeedback() {
+  const fb = useFeedback()
+  console.log('Submitting feedback:', feedback)
+  try {
+    fb.createFeedback(feedback)
+    resetForm()
+  } catch (error) {
+    console.error('Error submitting feedback:', error)
+  }
+}
+
+function resetForm() {
+  Object.assign(feedback, {
+    title: '',
+    body: ''
+  })
+  isOpen.value = !isOpen.value
+}
 </script>
 
 <style scoped></style>
--- src/runtime/components/KanbanBoard.vue
+++ src/runtime/components/KanbanBoard.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="flex justify-start">
+    <!-- !TODO: Discuss about this approach, it might be better to have a KanbanColumn component
+    then our users can choose to only display on column if required
+    -->
     <div
       v-for="(column, category) in columns"
       :key="category"
@@ -15,43 +18,43 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
 import { ref, onMounted } from 'vue'
 import KanbanCard from './KanbanCard.vue'
-import useGithub from '../composables/useGithub'
+import { useFeedback } from '../composables/useFeedback'
+import { OldFeedback } from '../types/feedback'
 
-export default {
-  components: {
-    KanbanCard
-  },
-  props: {
-    feedback: Array
-  },
-  setup(props) {
-    const { getAllCards } = useGithub()
-    const feedback = ref(props.feedback)
 
-    onMounted(async () => {
-      try {
-        const githubData = await getAllCards()
-        feedback.value = githubData
-        console.log('Fetched GitHub data:', githubData)
-        for (const item of githubData) {
-          const category = item.fieldValues.nodes[1]?.name || 'Draft'
-          console.log('category', category)
-          if (!columns.value[category]) {
-            columns.value[category] = []
-          }
-          columns.value[category].push(item)
-        }
-      } catch (error) {
-        console.error('Error fetching GitHub data:', error)
-      }
-    })
-    const columns = ref({})
-    return {
-      columns
-    }
+const p = defineProps({
+  feedback: {
+    type: Object as OldFeedback,
+    required: true
   }
-}
+})
+
+const { getFeedback } = useFeedback()
+const feedback = ref(p.feedback)
+const columns = ref({})
+
+onMounted(async () => {
+  try {
+    // !TODO: discuss if we should fetch the feedback of leave that upto our users
+    // state is also something to think about
+    const githubData = await getFeedback()
+    feedback.value = githubData
+    console.log('Fetched GitHub data:', githubData)
+    // !TODO avoid processing the data in components, do that in the helper folder
+    for (const item of githubData) {
+      const category = item.fieldValues.nodes[1]?.name || 'Draft'
+      console.log('category', category)
+      if (!columns.value[category]) {
+        columns.value[category] = []
+      }
+      columns.value[category].push(item)
+    }
+  } catch (error) {
+    console.error('Error fetching GitHub data:', error)
+  }
+})
+
 </script>
