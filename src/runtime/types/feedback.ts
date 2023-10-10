e8f40ba731f8573eff930a6c0cab3bc5d0c4b1ed
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-10-10 20:24:51 +0530

    chore(types): move dir

--- src/runtime/types/feedback.ts
+++ src/runtime/types/feedback.ts
@@ -1,13 +1,13 @@
 import { z } from 'zod'
 
-export const userFeedbackSchema = z.object({
+export const newFeedbackSchema = z.object({
   title: z.string(),
   body: z.string(),
   user_id: z.string().optional(),
   category_id: z.number()
 })
 
-export const feedbackSchema = userFeedbackSchema.extend({
+export const oldFeedbackSchema = newFeedbackSchema.extend({
   created_at: z.string().optional(),
   updated_at: z.string().optional(),
   votes_weighted: z.number(),
@@ -19,5 +19,5 @@ export const feedbackSchema = userFeedbackSchema.extend({
   votes: z.number()
 })
 
-export type Feedback = z.infer<typeof feedbackSchema>
-export type UserFeedback = z.infer<typeof userFeedbackSchema>
+export type OldFeedback = z.infer<typeof oldFeedbackSchema>
+export type NewFeedback = z.infer<typeof newFeedbackSchema>
