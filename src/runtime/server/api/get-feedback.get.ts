832968dc79258f4a04eb348d020033d73f2f3665
Drew MacGibbon <drewmacgibbon@gmail.com>
2023-10-25 00:20:52 +0530

    chore: testing endpoints with useStorage

--- src/runtime/server/api/get-feedback.get.ts
+++ src/runtime/server/api/get-feedback.get.ts
@@ -1,5 +1,6 @@
 // @ts-expect-error nitro aliases aren't registered
-import { useRuntimeConfig } from '#internal/nitro'
+import { useStorage } from '#imports'
+import { snapshot } from 'unstorage'
 import { defineEventHandler, createError } from 'h3'
 import { ghClient } from '../utils/ghClient'
 import gqlQuery from '../../gql/query'
@@ -7,26 +8,34 @@ import { oldFeedbackArraySchema } from '../../types/feedback'
 
 export default defineEventHandler(async () => {
   console.log('Get Feedback:')
-  const gh = useRuntimeConfig().feedback.github
-  const client = ghClient()
+  // const gh = useRuntimeConfig().feedback.github
+  // const client = ghClient()
+  const storage = useStorage('feedback')
   // Validate the incoming data
 
   try {
-    console.log('Project ID:', gh.project_id)
-    const response = await client.graphql(gqlQuery.projectIssues, {
-      projectId: gh.project_id
+    const data = await snapshot(storage)
+    console.log('snapshot', data)
+    const feedbackArray = []
+    Object.keys(data).forEach((key) => {
+      console.log('key', key, data[key])
+      feedbackArray.push(data[key])
     })
-    const cards = response?.node?.items?.nodes || []
+    console.log('feedbackArray', feedbackArray)
+    // const response = await client.graphql(gqlQuery.projectIssues, {
+    //   projectId: gh.project_id
+    // })
+    // const cards = response?.node?.items?.nodes || []
 
-    const formattedCards = cards.map((card) => {
-      console.log('Card:', card)
-      return card.content
-    })
+    // const formattedCards = cards.map((card) => {
+    //   return card.content
+    // })
 
     return {
       status: 200,
       message: 'Feedback Fetched',
-      body: oldFeedbackArraySchema.parse(formattedCards)
+      body: feedbackArray
+      // body: oldFeedbackArraySchema.parse(formattedCards)
     }
   } catch (error: any) {
     throw createError({
--- src/runtime/server/api/post-feedback.post.ts
+++ src/runtime/server/api/post-feedback.post.ts
@@ -1,26 +1,37 @@
 // @ts-expect-error nitro aliases aren't registered
-import { useRuntimeConfig } from '#internal/nitro'
+import { useStorage } from '#imports'
 import { defineEventHandler, readBody, createError } from 'h3'
 import { ghClient } from '../utils/ghClient'
 import gqlCreate from '../../gql/create'
 
+interface Feedback {
+  title: string
+  body: string
+}
+
 export default defineEventHandler(async (event) => {
   const newFeedback = await readBody(event)
   console.log('Enquiry:', newFeedback)
-    const gh = useRuntimeConfig().feedback.github
-
-  const client = ghClient()
-  const variables = {
-    title: newFeedback.title,
-    body: newFeedback.body,
-    projectId: gh.project_id
-  }
+  // const gh = useRuntimeConfig().feedback.github
+  // const client = ghClient()
+  // const variables = {
+  //   title: newFeedback.title,
+  //   body: newFeedback.body,
+  //   projectId: gh.project_id
+  // }
   // Validate the incoming data
-
+  const storage = useStorage('feedback')
+  console.log('storage', storage)
   try {
-    const response = await client.graphql(gqlCreate.draftIssue, variables)
-
-    console.log('New Feedback Created, ID:', response)
+    const doesFeedbackExist = await storage.hasItem<Feedback[]>('test')
+    if (!doesFeedbackExist) {
+      await storage.setItem<Feedback[]>('test', [newFeedback])
+    } else {
+      const currentFeedback = await storage.getItem<Feedback[]>('test')
+      currentFeedback.push(newFeedback)
+      await storage.setItem<Feedback[]>('test', currentFeedback)
+    }
+    // const response = await client.graphql(gqlCreate.draftIssue, variables)
 
     return {
       status: 200,
